8d883915bb89997a53b8daad61af7cea105b9300
Gasan <gasanalunkachev@gmail.com>
2026-06-29 11:05:02 +0300

    fix: add --accept-data-loss to prisma db push in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "[startup] Applying database schema (prisma db push)..."
-node /app/node_modules/prisma/build/index.js db push --skip-generate
+node /app/node_modules/prisma/build/index.js db push --skip-generate --accept-data-loss
 
 echo "[startup] Schema applied. Starting Next.js..."
 exec node server.js
